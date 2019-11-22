237232bce3f89a8b06c0b921cac490c2a69350bc
Andrew Noblet <andrewbnoblet@gmail.com>
2019-11-21 22:12:55 -0500

    Chore: Save

--- src/models/page.ts
+++
@@ -1,3 +0,0 @@
-export class page {
-  title: string;
-}
--- src/page/list.ts
+++ src/page/list.ts
@@ -1,46 +1,87 @@
 import { LitElement, css, customElement, html, property } from "lit-element";
+import { deleteDocument, getCollection } from "@anoblet/firebase";
 
 import { BeforeRenderMixin } from "@anoblet/mixins";
-import { deleteDocument, getCollection } from "@anoblet/firebase";
+import page from "page";
 
 @customElement("page-list")
 class PageListComponent extends BeforeRenderMixin(LitElement) {
   @property() data;
 
   async beforeRender() {
-    const data = await getCollection("pages", {
+    await getCollection("pages", {
       callback: collection => (this.data = collection),
       orderBy: "sortOrder"
     });
-    console.log(data);
+  }
+  create() {
+    page("/page/create");
   }
 
-  deleteDocument(id) {
-    deleteDocument(`pages/${id}`);
+  delete(event: any) {
+    deleteDocument(`pages/${event.composedPath()[0].dataset.id}`);
   }
 
   static styles = css`
-    :host,
-    .grid {
+    :host {
+      display: grid;
+      grid-gap: 2rem;
+      box-sizing: border-box;
+    }
+
+    #table {
       display: grid;
       grid-gap: 1rem;
+    }
+
+    .row {
+      display: grid;
+      grid-template-columns: max-content auto max-content;
+      grid-gap: 1rem;
+    }
+
+    .row span {
+    }
+
+    button-component {
+      border: 1px solid #000;
+      padding: 0.5rem;
+      width: max-content;
+    }
+
+    #actions {
+
+    }
+
+    hr {
+      color: #000;
+      width: 100%;
     }
   `;
 
   render() {
     return html`
-      Pages:
-      <div class="grid">
+      <div id="actions">
+        <button-component @click=${this.create}>Create</button-component>
+      </div>
+      <div id="table">
+        <div class="row">
+          <span>#</span>
+          <span>Title</span>
+          <span>Actions</span>
+        </div>
         ${this.data.map(
           (page, index) => html`
-            <span>
-              ${index}. ${page.title} (<a href="/page/read/${page.slug}">View</a
-              >/<a href="/page/edit/${page.id}">Edit</a>/<a
-                href=""
-                @click=${() => this.deleteDocument(page.id)}
-                >Delete</a
-              >)
-            </span>
+            <div class="row">
+              <span> ${index}</span><span>${page.title}</span
+              ><span
+                ><a href="/${page.slug}">View</a>
+                <a href="/page/edit/${page.id}">Edit</a>
+                <a href="" data-id=${page.id} @click=${this.delete}
+                  >Delete</a
+                ></span
+              >
+            </div>
           `
         )}
       </div>
