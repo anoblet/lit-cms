f14574a432e009c5b32846514a62564c13f516c6
Andrew Noblet <andrewbnoblet@gmail.com>
2019-12-01 17:24:24 -0500

    Chore: Save

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript";
 
+process.env.NODE_ENV = 'production';
+
 module.exports = {
   input: "./src/index.ts",
   output: {
